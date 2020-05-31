d6c4388e1dc7712093f3669caa90c46fce402607
haniaroad <haniaroadlifestyle@gmail.com>
2020-05-31 02:15:20 +0000

    Creating user and course model along with navbar style enhancements

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13.0'
 
 gem 'simple_form'
+
+gem 'faker'
+
+gem 'devise'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "6fa49481d21b48f5a471d17e34cb6c8c.vfs.cloud9.us-west-2.amazonaws.com"
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
