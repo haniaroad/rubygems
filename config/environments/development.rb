fbfb5f131229d0543cecd9f8a48f70952c3ff619
haniaroad <haniaroadlifestyle@gmail.com>
2020-06-06 16:49:01 +0000

    Configuring to use SendGrid via Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   
   config.action_mailer.default_url_options = { host: 'https://6fa49481d21b48f5a471d17e34cb6c8c.vfs.cloud9.us-west-2.amazonaws.com/' }
 
+  config.action_mailer.delivery_method = :test
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
