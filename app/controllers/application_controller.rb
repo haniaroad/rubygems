bc88e4204c7657a62ff88b6874c1b8aea497d343
haniaroad <haniaroadlifestyle@gmail.com>
2020-06-15 15:34:00 +0000

    Fixed logout has_role undefined error and added abiliity to see if user is logged in or not via admin UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   
   before_action :authenticate_user!
+  after_action :user_activity
   
   include Pundit
   protect_from_forgery
@@ -12,6 +13,10 @@ class ApplicationController < ActionController::Base
   def user_not_authorized #pundit
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+  
+  def user_activity
+    current_user.try :touch
   end
   
   include PublicActivity::StoreController
